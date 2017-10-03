30b4caf5ba50a477d00386f087668852e7429c85
Jomit <jovagh@microsoft.com>
2017-10-03 11:32:07 -0700

    added regression sample code

---
+++ regression/regression-intro.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import quandl, math, datetime
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
+
+df = quandl.get("WIKI/GOOGL")
+#print(df.head())
+
+df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+
+df = df[['Adj. Close', 'HL_PCT','PCT_change','Adj. Volume']]
+#print(df.head())
+
+forecast_col = 'Adj. Close'
+df.fillna(-99999, inplace=True)
+
+forecast_out = int(math.ceil(0.01*len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+#print(df.head())
+#print(df.tail())
+
+X = np.array(df.drop(['label'],1)) # features are everything except the label
+X = preprocessing.scale(X)   #normailzes the data, adds more processing time
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
+df.dropna(inplace=True)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)  # 20% data for test
+
+clf = LinearRegression(n_jobs=-1)
+#clf = svm.SVR()   # easy to switch the algorithm
+clf.fit(X_train, y_train)
+
+accuracy = clf.score(X_test, y_test)
+#print(accuracy)
+
+forecast_set = clf.predict(X_lately)
+#print(forecast_set, accuracy, forecast_out)
+
+df['Forecast'] = np.nan
+
+## workaround as we do not have dates
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
+
